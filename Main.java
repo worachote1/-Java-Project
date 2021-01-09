b6f1568e76acae7a872c84cf2ad6bab5e4562e27
worachote1 <68600257+worachote1@users.noreply.github.com>
2021-01-09 10:36:59 +0700

    ๋Java เบื้องต้น 1
    ๋Java เบื้องต้น 1 test on github

--- Main.java
+++ Main.java
@@ -1,44 +1,27 @@
 package com.company;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
-
-
 public class Main {
-    public static int KMTIL(int GPA, int Onet, int Gat, int pat2, int pat3) {
-        return GPA * 20 + Onet * 30 + Gat * 15 + pat2 * 15 + pat3 * 20;
-    }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+                System.out.println("time walker");
+                System.out.println(27);
+                System.out.println("----------------------------------------------------------------");
 
-        System.out.print("Enter your GPA : ");
-        int GPA = in.nextInt();
+                Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter your Onet : ");
-        int Onet = in.nextInt();
+                System.out.print("width equal to : ");
+                int width = in.nextInt();
+                System.out.println("width คือ " + width);
+                System.out.print("height equal to : ");
 
-        System.out.print("Enter your Gat: ");
-        int Gat = in.nextInt();
-
-        System.out.print("Enter your PAT2 : ");
-        int pat2 = in.nextInt();
-
-        System.out.print("Enter your PAT3 : ");
-        int pat3 = in.nextInt();
-
-        if (KMTIL(GPA, Onet, Gat, pat2, pat3) >= 19900) {
-            System.out.println(
-                    "Congrats !!! you can study engineering in KMTIL university" +
-                            "with your TCAS score :" + KMTIL(GPA, Onet, Gat, pat2, pat3));
-        } else {
-            System.out.println("Your TCAS score is " + KMTIL(GPA, Onet, Gat, pat2, pat3) +
-                    " is'nt reached to minimum score...." + "so keep trying !!!");
-
+                int height = in.nextInt();
+                System.out.println("height คือ " + height);
+                int area = width * height;
+                System.out.println("area คือ " + area);  }
         }
-    }
-}
+
+        // write your code here
+
 
